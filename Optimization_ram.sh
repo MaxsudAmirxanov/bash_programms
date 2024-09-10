12bee38e6d66a341ff6853a15a0d3cd06dbe9b11
MaxsudAmirxanov <maxsud@kali.maxsud>
2024-09-11 01:33:39 +0400

    create file for optimizateion ram

---
+++ Optimization_ram.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Проверка прав администратора
+if [ "$EUID" -ne 0 ]; then 
+  echo "Пожалуйста, запустите скрипт с правами администратора (root)"
+  exit
+fi
+
+# Оптимизация использования оперативной памяти
+
+echo "Начало оптимизации оперативной памяти..."
+
+# Освобождение кэша страниц
+echo 1 > /proc/sys/vm/drop_caches
+
+# Освобождение inode и dentry кэша
+echo 2 > /proc/sys/vm/drop_caches
+
+# Освобождение кэша страниц, inode и dentry
+echo 3 > /proc/sys/vm/drop_caches
+
+# Принудительный запуск процесса сжатия памяти (если поддерживается системой)
+if [[ -e /sys/kernel/mm/ksm/run ]]; then
+  echo 1 > /sys/kernel/mm/ksm/run
+fi
+
+# Удаление неиспользуемых данных из swap памяти
+swapoff -a && swapon -a
+
+echo "Оптимизация завершена. Проверка состояния памяти:"
+free -h
+
+# ps -e -o pid,ppid,cmd,%mem,%cpu,stat,start_time --sort=-%mem | head -5
+# cat /proc/meminfo | head -20 
--- temperature.sh
+++ temperature.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+
 while true; do
     echo "-------------------------------"
+    echo "$(date)"
     echo "$(sensors | grep 'Core\|temp1')"
-
+    
 
     echo "$(sensors | grep 'fan')"
-    sleep 60 
+    sleep 20 
+done
     
